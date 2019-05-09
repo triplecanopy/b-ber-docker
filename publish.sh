115778cadec2da599a8a2a5215506054daa1f7b9
Maxwell Simmer <maxwell.simmer@gmail.com>
2019-05-09 18:53:26 +0200

    Read proper image id

--- publish.sh
+++ publish.sh
@@ -8,7 +8,6 @@ fi
 
 IMAGE_NAME="canopycanopycanopy/b-ber"
 read -ra IMAGE_VERSION <<< "$(git describe --abbrev=0 --tags)"
-read -ra IMAGE_ID <<< "$(docker images --filter reference=$IMAGE_NAME -q)"
 read -ra VCS_REF <<< "$(git rev-parse --short HEAD)"
 read -ra VCS_URL <<< "$(git config --get remote.origin.url)"
 BUILD_DATE=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
@@ -18,7 +17,6 @@ echo "  Building:"
 echo
 echo "  Image Name: $IMAGE_NAME"
 echo "  Image Version: $IMAGE_VERSION"
-echo "  Image ID: $IMAGE_ID"
 echo "  VCS Ref: $VCS_REF"
 echo "  VCS URL: $VCS_URL"
 echo "  Build Date: $BUILD_DATE"
@@ -30,6 +28,9 @@ function deploy {
                  --build-arg BUILD_DATE="$BUILD_DATE" \
                  --build-arg IMAGE_VERSION="$IMAGE_VERSION" \
                  -t "$IMAGE_NAME":"$IMAGE_VERSION" .
+
+    read -ra IMAGE_ID <<< "$(docker images --filter reference=$IMAGE_NAME -q)"
+
     docker tag "$IMAGE_ID" "$IMAGE_NAME":"$IMAGE_VERSION"
     docker push "$IMAGE_NAME":"$IMAGE_VERSION"
 }
